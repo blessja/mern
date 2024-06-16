3740fc9fc690f7ecd1907650c90ad8eaba58f6a4
Blessing Jackson <jaydenj772@gmail.com>
2024-06-16 19:44:48 +0200

    changed main page

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -19,26 +19,22 @@ import Card from "../../components/Card/Card";
 const MainPage = () => {
   return (
     <>
-      <section
-        style={{ maxWidth: "100%", height: 2477 }}
-        className="main-content"
-      >
-        <div style={{ width: "100%" }} className=" container-fluid p-0 ">
+      <section className="main-content w-100" style={{ height: 2477 }}>
+        <div className="container-fluid p-0">
           <div
+            className="hero bg-overlay container-fluid d-flex flex-column align-items-center justify-content-center"
             style={{ height: "440px" }}
-            className="hero bg-overlay container-fluid d-flex flex-row align-items-center align-content-center"
           >
-            <div className=" container d-flex flex-column">
-              <div class="d-flex d-lg-none justify-content-between mt-3">
-                <button class="btn">
-                  <img src={arrowback.png} alt="back" />
+            <div className="container d-flex flex-column">
+              <div className="d-flex d-lg-none justify-content-between mt-3">
+                <button className="btn">
+                  <img src={arrowback} alt="back" />
                 </button>
-                <button class="btn group">Leave Group</button>
+                <button className="btn group">Leave Group</button>
               </div>
               <div>
-                {" "}
-                <h1 className=" text-white">Computer Engineering</h1>
-                <p className=" text-white mb-5">
+                <h1 className="text-white">Computer Engineering</h1>
+                <p className="text-white mb-5">
                   142,765 Computer Engineers follow this
                 </p>
               </div>
@@ -50,118 +46,71 @@ const MainPage = () => {
         </div>
 
         <section
-          style={{ maxWidth: 1040 }}
           className="py-3 py-md-5 py-xl-8 mx-auto"
+          style={{ maxWidth: 1040 }}
         >
-          <div
-            style={{ width: 1140 }}
-            className="container overflow-hidden gap-5 d-flex flex-row"
-          >
-            <Card />
+          <div className="container d-flex flex-column flex-lg-row gap-5">
+            <div className="flex-fill">
+              <Card />
+            </div>
 
             <div
+              className="d-flex flex-column align-items-center gy-4 gy-lg-0"
               style={{ width: 300 }}
-              className=" col mx-auto d-flex flex-column align-items-center gy-4 gy-lg-0"
             >
-              <div style={{ width: 234 }} className="d-flex flex-row">
-                <div className=" d-flex gap-2 align-items-center">
+              <div className="d-flex flex-row" style={{ width: 234 }}>
+                <div className="d-flex gap-2 align-items-center">
                   <FontAwesomeIcon icon={faLocationDot} />
                   <input
                     type="text"
                     placeholder="Enter Your Location"
-                    className=" input-text"
+                    className="form-control"
                   />
                 </div>
-                <button className=" border-0 bg-white">
-                  <FontAwesomeIcon
-                    icon={faPen}
-                    style={{ background: "#fff" }}
-                  />
+                <button className="btn bg-white border-0">
+                  <FontAwesomeIcon icon={faPen} />
                 </button>
               </div>
-              <div style={{ width: 243 }} className=" border border-1"></div>
-              <div style={{ width: 243 }} className="toolti mt-4 d-flex  gap-2">
+              <div className="border-top my-3" style={{ width: 243 }}></div>
+              <div className=" mt-4 d-flex gap-2" style={{ width: 243 }}>
                 <FontAwesomeIcon icon={faExclamationCircle} />
-                <p style={{ fontSize: 12 }}>
+                <p className="small">
                   Your location will help us serve better and extend a
                   personalised experience.
                 </p>
               </div>
-              <div className="recomended_groups d-flex gap-2 justify-content-around mt-2">
+              <div className="recommended-groups d-flex gap-2 justify-content-around mt-2">
                 <FontAwesomeIcon icon={faThumbsUp} />
                 <span className="text-recommended">Recommended Groups</span>
               </div>
-              <div
-                div
-                style={{ width: 243 }}
-                className=" d-flex flex-column gap-2"
-              >
-                <div className=" d-flex align-content-center justify-content-between  mt-4 gap-5">
-                  <div className=" d-flex align-items-center gap-2">
-                    <img src={leisure} alt="" />
-                    <div>Leisure</div>
+              <div className="d-flex flex-column gap-2" style={{ width: 243 }}>
+                {[
+                  { src: leisure, label: "Leisure" },
+                  { src: activism, label: "Activism" },
+                  { src: mba, label: "MBA" },
+                  { src: philosophy, label: "Philosophy" },
+                ].map((item, index) => (
+                  <div
+                    key={index}
+                    className="d-flex justify-content-between mt-2 gap-5"
+                  >
+                    <div className="d-flex align-items-center gap-2">
+                      <img src={item.src} alt={item.label} />
+                      <div>{item.label}</div>
+                    </div>
+                    <div>
+                      <a
+                        className="btn btn-outline-primary rounded-5 following d-flex align-items-center"
+                        href="/follow"
+                      >
+                        Follow
+                      </a>
+                    </div>
                   </div>
-                  <div className=" align-content-center">
-                    <a
-                      style={{ height: 24 }}
-                      className="btn rounded-5  following d-flex align-items-center"
-                      href="/follow"
-                    >
-                      Follow
-                    </a>
-                  </div>
-                </div>
-
-                <div className=" d-flex align-content-center justify-content-between  mt-2 gap-5">
-                  <div className=" d-flex align-items-center gap-2">
-                    <img src={activism} alt="" />
-                    <div>Activism</div>
-                  </div>
-                  <div className=" align-content-center">
-                    <a
-                      style={{ height: 24 }}
-                      className="btn rounded-5  following d-flex align-items-center"
-                      href="/follow"
-                    >
-                      Follow
-                    </a>
-                  </div>
-                </div>
-
-                <div className=" d-flex align-content-center justify-content-between  mt-2 gap-5">
-                  <div className=" d-flex align-items-center gap-2">
-                    <img src={mba} alt="" />
-                    <div>MBA</div>
-                  </div>
-                  <div className=" align-content-center">
-                    <a
-                      style={{ height: 24 }}
-                      className="btn rounded-5  following d-flex align-items-center"
-                      href="/follow"
-                    >
-                      Follow
-                    </a>
-                  </div>
-                </div>
-
-                <div className=" d-flex align-content-center justify-content-between  mt-2 gap-5">
-                  <div className=" d-flex align-items-center gap-2">
-                    <img src={philosophy} alt="" />
-                    <div>Philosophy</div>
-                  </div>
-                  <div className=" align-content-center">
-                    <a
-                      style={{ height: 24 }}
-                      className="btn rounded-5  following d-flex align-items-center"
-                      href="follow"
-                    >
-                      Follow
-                    </a>
-                  </div>
-                </div>
+                ))}
               </div>
-              <div className="see_more d-flex align-items-end">
-                <button className="btn text-end mt-5 text-primary text-capitalize">
+              <div className="see-more mt-5">
+                <button className="btn text-primary text-capitalize">
                   see more...
                 </button>
               </div>
